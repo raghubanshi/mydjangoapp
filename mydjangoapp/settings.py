a38d0c4f07c9856c6a1f877eb89993f2db562bc9
Praveen Raghubanshi <47377468+raghubanshi@users.noreply.github.com>
2024-07-13 20:02:15 -0500

    Update settings.py

--- mydjangoapp/settings.py
+++ mydjangoapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_dn)eqgp0_1rycyp1l_#qi&4-djb+qos$kj+ghd=+)u599mcqv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.15.187.93']
+ALLOWED_HOSTS = ['3.15.187.93', 'localhost', '127.0.0.1']
 
 
 # Application definition
