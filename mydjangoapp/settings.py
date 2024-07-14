0d96b3cf650148d9c26c4e305353dd0c9bd71edf
Praveen Raghubanshi <47377468+raghubanshi@users.noreply.github.com>
2024-07-13 20:14:19 -0500

    Update settings.py

--- mydjangoapp/settings.py
+++ mydjangoapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_dn)eqgp0_1rycyp1l_#qi&4-djb+qos$kj+ghd=+)u599mcqv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
